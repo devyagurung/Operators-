b5f8142d84cbe7e2286d105421195b99199ea0fb
class <class@CST_N417_09.cst.citytech.cuny.edu>
2015-02-23 17:20:40 -0500

    Operator!

---
+++ src/regular/wages/RegularWages.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package regular.wages;
+
+/**
+ *
+ * @author class
+ */
+public class RegularWages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO: Declare and initialize test scores as int.
+        int testScore1 = 90;
+        int testScore2 = 89;
+        int testScore3 = 90;
+                
+        //TODO: Display each test score, one per line
+        System.out.println( "The score for test 1 is : " + testScore1);
+        System.out.println( "The score for test 2 is : " + testScore2);
+        System.out.println( "The score for test 3 is : " + testScore3);
+        
+        
+        //TODO Calculate the average of the test scores.
+        double average = (testScore1 + testScore2 + testScore3)/3.0 ;
+        //TODO:
+        System.out.println("The average test score is: " + average);
+    
+        
+        
+        
+             
+             
+        
+        
+    }
+    
+}
